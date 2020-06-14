95617a120a0232752338148db4c91c549d70a5b9
luokai <kaiv2@outlook.com>
2020-06-14 20:51:47 +0800

    读取输入流

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,36 @@
 use qrcode::QrCode;
 use std::env;
+use std::io;
 fn main() {
     let mut args = env::args();
     args.next();
-    loop {
-        let qc = args.next();
-        let qcs = match qc {
-            Some(s) => s,
-            None => break,
-        };
-        println!();
-        println!("--------------------------------------------------");
-        println!("原始数据: {}", qcs);
-        println!("--------------------------------------------------");
-        println!();
-        let code = QrCode::new(&qcs).unwrap();
-        let qrcode = code
-            .render::<char>()
-            .quiet_zone(false)
-            .module_dimensions(2, 1)
-            .build();
-        println!("{}", qrcode);
+    let mut flag = true;
+    for qrc in args {
+        flag = false;
+        print_qrcode(&qrc);
     }
+    if flag {
+        let stdin = io::stdin();
+        let mut qrc = String::new();
+        if let Ok(n) = stdin.read_line(&mut qrc) {
+            if n > 0 && qrc != "\n" {
+                print_qrcode(&qrc);
+            }
+        }
+    }
+}
+
+fn print_qrcode(qcs: &str) {
+    println!();
+    println!("--------------------------------------------------");
+    println!("原始数据: {}", qcs);
+    println!("--------------------------------------------------");
+    println!();
+    let code = QrCode::new(qcs).unwrap();
+    let qrcode = code
+        .render::<char>()
+        .quiet_zone(false)
+        .module_dimensions(2, 1)
+        .build();
+    println!("{}", qrcode);
 }
